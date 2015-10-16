d2a16633fede9eb95e999b4ce3a65c6c2e2b6a94
Terry <terry@example.com>
2015-10-16 18:27:09 +0800

    go previous page after read NFC card

--- app/src/main/java/com/abc/terry_sun/abc/R_CardNoFriendCardActivity.java
+++ app/src/main/java/com/abc/terry_sun/abc/R_CardNoFriendCardActivity.java
@@ -1,30 +1,16 @@
 package com.abc.terry_sun.abc;
 
-import android.app.Fragment;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
-import android.util.Log;
-import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
-import android.widget.PopupMenu;
 
 import com.abc.terry_sun.abc.CustomClass.AsyncTask.AsyncTaskPostProcessingInterface;
 import com.abc.terry_sun.abc.CustomClass.AsyncTask.AsyncTaskProcessingInterface;
 import com.abc.terry_sun.abc.Entities.DB_Cards;
-import com.abc.terry_sun.abc.Models.GalleryItem;
 import com.abc.terry_sun.abc.Provider.VariableProvider;
 import com.abc.terry_sun.abc.Service.CardService;
-import com.abc.terry_sun.abc.Service.ProcessControlService;
 import com.abc.terry_sun.abc.Service.ServerCommunicationService;
-import com.facebook.appevents.AppEventsLogger;
 
 import butterknife.ButterKnife;
-import butterknife.InjectView;
-import butterknife.OnClick;
-import eu.livotov.zxscan.ScannerView;
 
 /**
  * Created by terry_sun on 2015/7/20.
@@ -32,14 +18,16 @@ import eu.livotov.zxscan.ScannerView;
 public class R_CardNoFriendCardActivity extends BasicActivity {
     String TAG="R_CardNoFriendCardActivity";
     static int NewCardID=0;
+    FragmentTransaction transaction;
+    CardReaderFragment fragment;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_r_card_no_friendcard);
         ButterKnife.inject(this);
         if (savedInstanceState == null) {
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-            CardReaderFragment fragment = new CardReaderFragment();
+            transaction = getSupportFragmentManager().beginTransaction();
+            fragment = new CardReaderFragment();
             fragment.SetAsyncTaskProcessingInterface(new AsyncTaskProcessingInterface() {
                 @Override
                 public void DoProcessing() {
@@ -55,8 +43,10 @@ public class R_CardNoFriendCardActivity extends BasicActivity {
                     DB_Cards _DB_Cards=CardService.getInstance().GetCardsByCardID(String.valueOf(NewCardID));
                     if(_DB_Cards!=null)
                     {
+                        TabGroup_R_Card.GoPreviousView();
                         CardService.getInstance().ShowCardDetailDialog(_DB_Cards.getEntityCardID(), MainActivity.GetMainActivityContext());
                     }
+
                 }
             });
             transaction.replace(R.id.fragmentlayout_readcard, fragment);
