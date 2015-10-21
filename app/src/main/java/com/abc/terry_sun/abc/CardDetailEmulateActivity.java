ccf08bb5f551cbe939273eccfc676a475e7e9329
Terry <terry@example.com>
2015-10-21 16:04:21 +0800

    remove useless file

--- app/src/main/java/com/abc/terry_sun/abc/CardDetailEmulateActivity.java
+++ app/src/main/java/com/abc/terry_sun/abc/CardDetailEmulateActivity.java
@@ -1,19 +1,7 @@
 package com.abc.terry_sun.abc;
 
 import android.app.Activity;
-import android.app.TabActivity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.TabHost;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.abc.terry_sun.abc.CustomClass.Adapter.AdapterCardsImage;
 
 /**
  * Created by terry_sun on 2015/6/2.
@@ -21,7 +9,6 @@ import com.abc.terry_sun.abc.CustomClass.Adapter.AdapterCardsImage;
 public class CardDetailEmulateActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_card_detail_emulate);
     }
 
 }
