cee83f921bb51ac1bbfa5a885e7b3b7a52954cf8
Terry <terry@example.com>
2016-01-12 12:39:08 +0800

    change web server to azure

--- app/src/main/java/com/abc/terry_sun/abc/Provider/HttpURL_Provider.java
+++ app/src/main/java/com/abc/terry_sun/abc/Provider/HttpURL_Provider.java
@@ -4,7 +4,8 @@ package com.abc.terry_sun.abc.Provider;
  * Created by terry_sun on 2015/6/26.
  */
 public class HttpURL_Provider {
-    public static String ServerURL= "http://10.9.112.20:777/";
+    //public static String ServerURL= "http://10.9.112.20:777/";
+    public static String ServerURL= "http://abc-web.azurewebsites.net/";
     public static final String ImageServerLocation= ServerURL + "Cards/";
     public static final String FacebookLogin= ServerURL + "WebService/FacebookLogin";
     public static final String GetUserCard= ServerURL + "WebService/GetUserCard";
