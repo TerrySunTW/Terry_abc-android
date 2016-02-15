eb73fea39531c460ab56d2de96119146f42880d4
Terry <terry@example.com>
2016-02-15 16:49:49 +0800

    adjust Menu UI

--- app/src/main/java/com/abc/terry_sun/abc/FriendsListActivity.java
+++ app/src/main/java/com/abc/terry_sun/abc/FriendsListActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -36,8 +37,8 @@ public class FriendsListActivity extends Activity {
 	@InjectView(R.id.FriendButton)
 	ImageButton FriendButton;
 
-	@InjectView(R.id.ButtonAddFriend)
-	BootstrapButton ButtonAddFriend;
+	@InjectView(R.id.ButtonAddFriendID)
+	Button ButtonAddFriend;
 
 	final String TAG="FriendsListActivity";
 	static Adapter_FriendsList _Adapter_FriendsList;
