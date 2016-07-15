f8da0e20aa26311a9a896dc68c9efdbb3bac54e4
Terry <terry@example.com>
2016-07-15 19:55:20 +0800

    change version

--- app/src/main/java/com/abc/terry_sun/abc/Provider/VersionProvider.java
+++ app/src/main/java/com/abc/terry_sun/abc/Provider/VersionProvider.java
@@ -4,5 +4,5 @@ package com.abc.terry_sun.abc.Provider;
  * Created by terry_sun on 2015/10/5.
  */
 public class VersionProvider {
-    public static final String CurrentVersion="0.1";
+    public static final String CurrentVersion="0.2";
 }
