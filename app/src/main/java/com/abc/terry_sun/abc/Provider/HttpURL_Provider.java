a98a9e906f721556e2e754df047852dce4f6c5de
Terry <terry@example.com>
2016-07-26 14:45:14 +0800

    adjust APP icon

--- app/src/main/java/com/abc/terry_sun/abc/Provider/HttpURL_Provider.java
+++ app/src/main/java/com/abc/terry_sun/abc/Provider/HttpURL_Provider.java
@@ -12,9 +12,7 @@ public class HttpURL_Provider {
     public static final String ToggleFavoriteSetting= ServerURL + "WebService/SetFavoriteCard";
     public static final String AddNewCard= ServerURL + "WebService/UserAddNewCard";
     public static final String AddFriendEntityCard= ServerURL + "WebService/AddFriendEntityCard";
-    public static final String AddFriendEntityQRCard= ServerURL + "WebService/AddFriendEntityQRCard";
     public static final String AddFriendNFCCard= ServerURL + "WebService/AddFriendNFCCard";
-    public static final String AddFriendRealCard= ServerURL + "WebService/UserAddFriendRealCard";
     public static final String GetUserCardIDByEntityID= ServerURL + "WebService/GetUserCardIDByEntityID";
     public static final String GetUserEvent= ServerURL + "WebService/GetUserEvent";
     public static final String SetMainCardSetting= ServerURL + "WebService/SetMainCard";
